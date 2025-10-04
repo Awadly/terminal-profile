feeda1f752136befb630cd16b18af647d4aa6af1
Awadly <abdelrahmanalawadly@gmail.com>
2025-10-04 22:10:10 +0300

    Removing unused pics and modifying metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Awadly Profile',
+  description: 'This is terminal profile of Awadly',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
